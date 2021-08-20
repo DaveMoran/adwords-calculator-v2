ab65f17ee346dfbd7b9c3cd0bc71ad93191684a2
Dave Moran <dave.moran118@gmail.com>
2021-08-20 09:15:07 -0400

    Add accounts to server

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const http = require('http')
 
+
+let accounts = [
+  {
+    "id": 3,
+    "name": "red",
+    "desiredSpend": 1500,
+    "currSpend": 998,
+    "newDaily": 45.63
+  },
+  {
+    "id": 4,
+    "name": "blue",
+    "desiredSpend": 1500,
+    "currSpend": 1985,
+    "newDaily": 0
+  }
+]
+
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello world')
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(accounts))
 })
 
 const PORT = 3001
