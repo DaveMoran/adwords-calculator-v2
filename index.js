59a5e3d4ad07b040cda34d7d3df3448cfe70a070
Dave Moran <dave.moran118@gmail.com>
2021-08-20 10:02:07 -0400

    Add new middleware to app

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:',  request.method)
+  console.log('Path:',    request.ppath)
+  console.log('Body:',    request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
 
 let accounts = [
   {
@@ -73,6 +82,12 @@ app.post('/api/accounts', (request, response) => {
   accounts = accounts.concat(account)
   response.json(account)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send(({ error: 'Unknown endpoint.'}))
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
