8abd8ce697a9ccd31480b6f2eb89cbd096e9b51e
Dave Moran <dave.moran118@gmail.com>
2021-08-20 12:32:34 -0400

    Add call to build folder

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 
 app.use(express.json())
 
+app.use(express.static('build'))
+
 const requestLogger = (request, response, next) => {
   console.log('Method:',  request.method)
   console.log('Path:',    request.ppath)
--- src/services/accounts.js
+++ src/services/accounts.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/accounts'
+const baseUrl = '/api/accounts'
 
 const getAll = () => {
   return axios.get(baseUrl)
