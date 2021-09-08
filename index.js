aecc509048fa35816cb40aac0d280938012cd555
Dave Moran <dave.moran118@gmail.com>
2021-09-08 12:04:41 -0400

    Update index to rely on new mongo connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors = require('cors')
+const Account = require('./models/account')
 
 app.use(express.json())
 
@@ -15,101 +16,42 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
-app.use(cors())
-
-let accounts = [
-  {
-    "id": 3,
-    "name": "red",
-    "desiredSpend": 1500,
-    "currSpend": 998,
-    "newDaily": 45.63
-  },
-  {
-    "id": 4,
-    "name": "blue",
-    "desiredSpend": 1500,
-    "currSpend": 1985,
-    "newDaily": 0
-  }
-]
-
-let profile = {
-  "startingBudget": 5500
-}
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>')
-})
 
 app.get('/api/accounts', (request, response) => {
-  response.json(accounts)
+  Account.find({}).then(accounts => {
+    response.json(accounts)
+  })
 })
 
 app.get('/api/accounts/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const account = accounts.find(account => account.id === id)
-
-  if(account) {
+  Account.findById(request.params.id).then(account => {
     response.json(account)
-  } else {
-    response.status(404).end()
-  }
-  
+  })
 })
 
 app.delete('/api/accounts/:id', (request, response) => {
-  const id = Number(request.params.id)
-  accounts = accounts.filter(account => account.id !== id)
-
-  response.status(204).end()
+  Account.findByIdAndRemove(request.params.id).then(account => {
+    response.json(account)
+  })
 })
-
-const generateId = () => {
-  const maxId = accounts.length > 0
-    ? Math.max(...accounts.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/accounts', (request, response) => {
   const body = request.body
 
   if(!body.name) {
-    return response.status(400).json({
-      error: 'Name missing from body'
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const account = {
+  const account = new Account({
     name: body.name,
     currSpend: body.currSpend || 0,
     desiredSpend: body.desiredSpend || 0,
-    id: generateId()
-  }
+    newDaily: body.newDaily || 0,
+  })
 
-  accounts = accounts.concat(account)
-  response.json(account)
-})
-
-app.get('/api/profile', (request, response) => {
-  response.json(profile)
-})
-
-app.patch('/api/profile', (request, response) => {
-  const body = request.body
-
-  if (!body.startingBudget) {
-    return response.status(400).json({
-      error: 'Starting Budget missing from body'
-    })
-  }
-
-  profile = {
-    "startingBudget": body.startingBudget
-  }
-
-  response.json(profile)
+  account.save().then(savedAccount => {
+    response.json(savedAccount)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -118,7 +60,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
